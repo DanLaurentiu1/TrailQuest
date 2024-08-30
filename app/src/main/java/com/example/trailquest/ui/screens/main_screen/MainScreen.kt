6d3a5099617cca2217d28844f6e86b309a2cb6c5
DanLaurentiu <laurentiu.dan1@stud.ubbcluj.ro>
2024-08-30 20:25:54 +0300

    added MainScreen and DataSources

--- app/src/main/java/com/example/trailquest/ui/screens/main_screen/MainScreen.kt
+++ app/src/main/java/com/example/trailquest/ui/screens/main_screen/MainScreen.kt
@@ -1,60 +1,46 @@
 package com.example.trailquest.ui.screens.main_screen
 
-import android.graphics.Color
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.modifier.ModifierLocalProvider
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.trailquest.R
-import com.example.trailquest.ui.theme.AppTypography
+import com.example.trailquest.data.datasource.DataSources
+import com.example.trailquest.ui.theme.TrailQuestAppTheme
 
 
 @Composable
 fun MainScreen() {
-    val types = listOf("Restaurants", "Zoos", "Beaches", "Parks", "Lakes")
-    Column(modifier = Modifier.fillMaxSize()) {
-        AppBar(
-            modifier = Modifier
-        )
-        FilterButtons(modifier = Modifier, types = types)
+    TrailQuestAppTheme {
+        Column(modifier = Modifier.fillMaxSize()) {
+            AppBar(
+                modifier = Modifier
+            )
+            FilterButtons(modifier = Modifier)
+        }
     }
 }
 
@@ -65,8 +51,8 @@ fun AppBar(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxWidth()
             .padding(
-                start = dimensionResource(R.dimen.padding_very_small),
-                top = dimensionResource(R.dimen.padding_very_small),
+                start = dimensionResource(R.dimen.padding_small),
+                top = dimensionResource(R.dimen.padding_medium),
             ),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
@@ -84,7 +70,7 @@ fun AppBar(modifier: Modifier = Modifier) {
                 .height(dimensionResource(R.dimen.search_bar_height)),
             leadingIcon = {
                 Icon(
-                    painter = painterResource(R.drawable.baseline_search_24),
+                    painter = painterResource(R.drawable.rounded_search_24),
                     contentDescription = ""
                 )
             },
@@ -120,18 +106,18 @@ fun FilterButton(modifier: Modifier = Modifier, text: String) {
 }
 
 @Composable
-fun FilterButtons(modifier: Modifier = Modifier, types: List<String>) {
+fun FilterButtons(modifier: Modifier = Modifier) {
     LazyRow(
         modifier = modifier
             .fillMaxWidth()
             .padding(
                 start = dimensionResource(R.dimen.padding_very_small),
-                top = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_very_high),
                 end = dimensionResource(R.dimen.padding_very_small),
             ),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        items(types) { item ->
+        items(DataSources.types) { item ->
             FilterButton(text = item)
         }
     }

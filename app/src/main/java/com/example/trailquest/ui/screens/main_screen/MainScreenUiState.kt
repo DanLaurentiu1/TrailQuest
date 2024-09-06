bde23771250e46c55b17b3ca6ca0a647a2ae9811
DanLaurentiu <laurentiu.dan1@stud.ubbcluj.ro>
2024-09-06 16:29:45 +0300

    added uiState and ViewModel for MainScreen

--- app/src/main/java/com/example/trailquest/ui/screens/main_screen/MainScreenUiState.kt
+++ app/src/main/java/com/example/trailquest/ui/screens/main_screen/MainScreenUiState.kt
@@ -1,2 +1,9 @@
 package com.example.trailquest.ui.screens.main_screen
 
+data class MainScreenUiState(
+    val isSearchBarActive: Boolean = false,
+    val isHomeSelected: Boolean = false,
+    val isStatisticsSelected: Boolean = false,
+    val isProfileSelected: Boolean = false,
+    val searchBarQuery: String = ""
+)
--- app/src/main/java/com/example/trailquest/ui/screens/main_screen/MainScreenViewModel.kt
+++ app/src/main/java/com/example/trailquest/ui/screens/main_screen/MainScreenViewModel.kt
@@ -1,2 +1,37 @@
 package com.example.trailquest.ui.screens.main_screen
 
+import androidx.lifecycle.ViewModel
+import com.example.trailquest.data.datasource.DataSources
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+class MainScreenViewModel : ViewModel() {
+    var uiState = MutableStateFlow(MainScreenUiState())
+        private set
+
+    init {
+        //Database
+        uiState.value = MainScreenUiState()
+    }
+
+    fun updateSearchBarQuery(newQuery: String) {
+        uiState.update {
+            it.copy(
+                searchBarQuery = newQuery
+            )
+        }
+    }
+
+    fun searchClick(): Boolean {
+        if (this.uiState.value.searchBarQuery in DataSources.countries) {
+            return true
+        } else {
+            resetSearchBarQuery()
+            return false
+        }
+    }
+
+    private fun resetSearchBarQuery() {
+        updateSearchBarQuery("")
+    }
+}
